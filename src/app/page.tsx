75ac48df9fe55825026df1b1b0cec557561f092c
NOSI / PM - Carlos Alberto Andrade  Graca <carlosa.graca@nosi.cv>
2025-09-09 23:59:34 -0100

    refactor(page.tsx): simplify root path assignment for clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import Dashboard from '@/components/dashboard';
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-  const root = process.env.IGRP_APP_HOME_SLUG ?? process.env.IGRP_APP_HOME_SLUG ?? '/';
+  const root = process.env.IGRP_APP_HOME_SLUG ? process.env.IGRP_APP_HOME_SLUG : '/';
+
   if (!root.startsWith('/')) throw new Error('Root redirect must be a valid path');
   if (root === '/') {
     return <Dashboard></Dashboard>;

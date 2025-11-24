9a376bcb08000a7cac21b881fecdf5f5b38f1ac5
NOSI / PM - Carlos Alberto Andrade  Graca <carlosa.graca@nosi.cv>
2025-11-24 00:25:18 -0100

    feat(page): update myTasks (mytasks) - 1763947518876

--- src/app/(igrp)/(generated)/mytasks/page.tsx
+++ src/app/(igrp)/(generated)/mytasks/page.tsx
@@ -147,7 +147,7 @@ router.push(taskUrl);
 </div>
 </IGRPPageHeader>
 
-<div className={ cn('grid','md:grid-cols-2 ','lg:grid-cols-3 ','xl:grid-cols-5 ',' gap-4',)}    >
+<div className={ cn('grid','md:grid-cols-2 ','lg:grid-cols-4 ','xl:grid-cols-5 ',' gap-4',)}    >
 	<IGRPStatsCard
   id={ `statsCard1` }
   cardBorderPosition={ `top` }

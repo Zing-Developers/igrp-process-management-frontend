761a889c5d84dd1b6426d7c9d120682ab3a69b25
maxwell <cubanmaxramos@gmail.com>
2026-04-20 08:46:10 -0100

    adding header integration

--- src/lib/url.ts
+++ src/lib/url.ts
@@ -1,5 +1,8 @@
 export const normalizeURL = (url: string) => url.replace(/\/\//g, "/");
 
+const appBasePath =
+  process.env.IGRP_APP_BASE_PATH ?? process.env.NEXT_PUBLIC_BASE_PATH ?? "";
+
 /**
  * Ideal for building public routes
  * @param assetName
@@ -7,3 +10,10 @@ export const normalizeURL = (url: string) => url.replace(/\/\//g, "/");
  */
 export const buildPublicUrlResource = (assetName: string) =>
   normalizeURL(`/${process.env.IGRP_APP_BASE_PATH ?? ""}/${assetName}`);
+
+export const withBasePath = (path: string) => {
+  const normalizedBasePath = appBasePath.endsWith("/")
+    ? appBasePath.slice(0, -1)
+    : appBasePath;
+  return `${normalizedBasePath}${path}`;
+};

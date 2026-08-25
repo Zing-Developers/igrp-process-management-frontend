0e0f9af843412f640d43a295a67e67524b3d8048
maxwell <cubanmaxramos@gmail.com>
2026-08-25 09:17:10 -0100

    submiting pages

--- src/app/(igrp)/layout.tsx
+++ src/app/(igrp)/layout.tsx
@@ -48,7 +48,6 @@ export default async function IGRPRootLayout({
   //   </IGRPLayout>
   // );
 
-
   return (
     <IRNLayout>
       <main className="px-4">
--- src/app/(myapp)/components/PageHeader.tsx
+++ src/app/(myapp)/components/PageHeader.tsx
@@ -13,7 +13,9 @@ export function PageHeader({
     <div className="py-4 grid gap-3 md:flex md:justify-between md:items-center">
       <div>
         <div className="inline-flex gap-x-2">
-          <h2 className="text-xl font-semibold text-gray-800 dark:text-neutral-200">{name}</h2>
+          <h2 className="text-xl font-semibold text-gray-800 dark:text-neutral-200">
+            {name}
+          </h2>
           {badgeCount !== undefined && badgeCount > 0 && (
             <span className="inline-flex items-center justify-center px-2 py-1 text-xs font-medium rounded-full bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-300">
               {badgeCount}
@@ -21,7 +23,9 @@ export function PageHeader({
           )}
         </div>
 
-        <p className="text-sm text-gray-600 dark:text-neutral-400">{description}</p>
+        <p className="text-sm text-gray-600 dark:text-neutral-400">
+          {description}
+        </p>
       </div>
 
       <div>{children}</div>
